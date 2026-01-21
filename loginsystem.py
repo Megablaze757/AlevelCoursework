1e6d15638408c84098ca8e55169da8fb6a90bb1a
Megablaze757 <77020416+Megablaze757@users.noreply.github.com>
2026-01-21 14:11:33 +0000

    Update loginsystem.py

--- loginsystem.py
+++ loginsystem.py
@@ -1,1 +1,12 @@
+from pyscript import document
+
+def handle_click(event):  
+  #Access the HTML element for the username
+
+
+
+  # Assign the form value to the username variable
+  username =  document.querySelector(Username)
+  password = document.querySelector(Password)
+  print('username')
 
